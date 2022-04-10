c5d8fc00c358ea6543ab22c19ae84311ce14854d
hasan-ullah <mdhasanullah124@gmail.com>
2022-04-10 12:40:29 +0600

    protected route , required auth create

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Login from './components/login/Login';
+import Orders from './components/orders/Orders';
 import Register from './components/register/Resister';
+import RequireAuth from './components/requireAuth/RequireAuth';
 
 const App = () => {
     return (
@@ -13,6 +15,11 @@ const App = () => {
                 <Route path='/' element={<Home></Home>}></Route>
                 <Route path='/login' element={<Login></Login>}></Route>
                 <Route path='/register' element={<Register></Register>}></Route>
+                <Route path='/orders' element={
+                    <RequireAuth>
+                        <Orders></Orders>
+                    </RequireAuth>
+                }></Route>
             </Routes>
         </div>
     );
