e2da766c278d02408ffa1fbc8b2cebb3f5c2da7b
hasan-ullah <mdhasanullah124@gmail.com>
2022-04-10 08:45:32 +0600

    google signin and custom hook make and use for login and sign out

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const {signInWithGoogle}= useFirebase()
     return (
         <div>
             <h2>from login</h2>
+            <div style={{margin:'20px'}}>
+                    <button onClick={signInWithGoogle}>google signin</button>
+            </div>
             <form>
-                <div style={{margin:'20px'}}>
-                    <button>google signin</button>
-                </div>
-                <input type="email" placeholder='email'/>
+               <input type="email" placeholder='email'/>
                 <br />
                 <input type="password" placeholder='password' />
                 <br />
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,17 +1,29 @@
 import { useEffect, useState } from "react"
+import app from "../firebase.init"
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const auth = getAuth(app)
+const googleProvider = new GoogleAuthProvider()
 
 const useFirebase=()=>{
     const [user, setUser]=useState({})
 
-     useEffect(()=>{
-
-     },[])
-    
+  
      const signInWithGoogle=()=>{
-         console.log('signin ok')
+         signInWithPopup(auth,googleProvider)
+         .then(result=>{
+             const user = result.user;
+             setUser(user)
+             console.log(user)
+         })
+         .catch()
      }
 
-     return [user, setUser];
+    //  return [user, setUser];
+       return {
+           user, 
+           signInWithGoogle
+       }    
 
 }
 export default useFirebase;
